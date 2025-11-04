b0c007b988a5e6cb36cab6cc29cc8658cc1e8703
Geliiil <ge268413975@gmail.com>
2025-11-04 14:59:50 +0800

    create a file  that can fetch data from NCBI

---
+++ fetch_data.py
@@ -0,0 +1,45 @@
+import requests
+from xml.etree.ElementTree import fromstring, ElementTree
+import os
+import time
+from tqdm import tqdm
+os.chdir("/Users/jinwenyu/Desktop/ncbi_data/data") #data folder path
+def fetch_data(name,Accessions):
+  IDs =[]
+  ncbi_url = "https://www.ncbi.nlm.nih.gov/sviewer/viewer.cgi?"
+  #get GI
+  for ac in Accessions:
+    r = requests.get(f"https://www.ncbi.nlm.nih.gov/nuccore/{ac}?report=gilist&log$=seqview&format=text")
+    tree = ElementTree(fromstring(r.text))
+    GI = tree.getroot().text.replace("\n","")
+    IDs.append(GI)
+    time.sleep(1.5)
+  print(IDs) 
+  #get data
+  params ={
+    'tool':'portal',
+    'save':'file',
+    'db':'nuccore',
+    'report':'gene_fasta',
+    'id' : ",".join(IDs)
+  }
+  response = requests.get(ncbi_url,params=params)
+  print("fetch:", name)
+  with open(f"{name}.txt","w") as f:
+    f.write(response.text)
+
+# read xlsx
+import pandas as pd
+path = "/Users/jinwenyu/Desktop/ncbi_data/NCBI-網路爬蟲測試.xlsx" #file (xlsx) path
+df = pd.read_excel(path)
+pbar = tqdm(total=len(df),desc="Fetching data...")
+for i in range(len(df)):
+  name=""
+  Accessions = []
+  for j in range(len(df.iloc[i])):
+    if j == 0:
+      name = df.iloc[i,j]
+    else:
+      Accessions.append(df.iloc[i,j])
+  fetch_data(name,Accessions)
+  pbar.update(1)
